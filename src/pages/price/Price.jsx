2306e9859d08019b2b7a27afe2f5da52cb1ba41a
badriyassine <yassine.badrii18@gmail.com>
2026-02-19 03:01:24 +0000

    update wifi and price pages to include satellite offers

--- src/pages/price/Price.jsx
+++ src/pages/price/Price.jsx
@@ -4,12 +4,14 @@ import MobileOffers from "./components/MobileOffers"
 import Supports from "../../pages/home/components/Supports"
 import Entertainment from "../home/components/Entertainment"
 import SatelliteOffers from "./../sat/SatelliteOffers"
+import Adsl from "./../wifi/components/Adsl"
 
 const Price = () => {
   return (
     <div className="">
       <Offers/>
       <FiveGOffers/>
+      <Adsl/>
       <MobileOffers/>
       <SatelliteOffers/>
       <Entertainment/>
